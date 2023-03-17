296e6ac27e9213b3c74504488844bcb5938d466c
SOUAADJIA <souadouledjia@gmail.com>
2023-03-17 16:50:06 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Seccuss)
+ */
+int main(void)
+{
+	int nbr = 48;
+
+	while (nbr <= 57)
+	{
+		putchar(nbr);
+		nbr++;
+	}
+	putchar('\n');
+	return (0);
+}
