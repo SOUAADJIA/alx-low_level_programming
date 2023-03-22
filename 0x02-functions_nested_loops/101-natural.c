7ee2464b930382318a9e4441717206004987f122
SOUAADJIA <souadouledjia@gmail.com>
2023-03-22 15:19:12 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,6 @@
 /**
  * main - Lists all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
- * @s: the sum of all the multiples of 3 or 5 below 1024 (excluded)
  * Return: Always 0.
  */
 int main(void)
