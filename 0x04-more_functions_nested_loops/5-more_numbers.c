320b9bd09501e4bf9178eb733efaa8b584628442
SOUAADJIA <souadouledjia@gmail.com>
2023-03-23 17:38:40 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* more_numbers - prints 10 times the numbers, from 0 to 14
+* followed by a new line.
+* Return: void.
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
