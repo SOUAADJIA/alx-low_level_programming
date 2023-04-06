bbb445203dabeff6f8d52e86f67dab6d61be0b14
SOUAADJIA <souadouledjia@gmail.com>
2023-04-05 20:22:26 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_prime_number - function that returns 1 if the input integer is a 
+ * is_prime_number - function that returns 1 if the input integer is a
  * prime number, otherwise return 0
  *
  * @n: input integer
@@ -14,8 +14,34 @@ int is_prime_number(int n)
 		return (0);
 	else if ((n == 2) || (n == 3))
 		return (1);
+	/*checking if n is prime by starting with the smallest possible divisor 2*/
 	else
-		return (calculate_is_prime(n));
+		return (calculate_is_prime(n, 2));
 
+}
+/**
+ * calculate_is_prime - calculate is prime
+ * @n: integer input to check if prime
+ * @i: devisor to check with
+ *
+ * Return: function that returns 1 if the input integer is a
+ * prime number, otherwise return 0
+ */
 
+int calculate_is_prime(int n, int i)
+{
+	if (n < 2)
+		return (0);
+
+	else if ((n == 2) || (n == 3))
+		return (1);
+
+	else if ((n % i) == 0)
+		return (0);
+
+	else if ((i * i) > n)
+
+		return (1);
+	else
+		return (calculate_is_prime(n, i + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int calculate_square(int n, int i);
+int calculate_is_prime(int n, int i);
 
 #endif
 
