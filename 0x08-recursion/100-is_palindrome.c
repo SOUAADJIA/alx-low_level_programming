082fbcff1b16e9c3a77988f320c9fbd38c8a289f
SOUAADJIA <souadouledjia@gmail.com>
2023-04-06 16:41:29 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_palindrome - function that checks if a string is a palindrome 
+ * is_palindrome - function that checks if a string is a palindrome
  *
  * @s: pointer to string that we gonna check
  *
@@ -15,18 +15,27 @@ int is_palindrome(char *s)
 {
 	int len_s = _strlen_recursion(s);
 
-	if ((len_s == 0) || (len_s == 1)) /* if lenght of s is 0 or 1 is a palindrome*/
+	/* if lenght of s is 0 or 1 is a palindrome*/
+	if ((len_s == 0) || (len_s == 1))
 		return (1);
-	else 
-		return (is_palindrome_helper(s, 0, len_s - 1));
+	else
+		return (is_palindrome_helper(s, 0, len_s - 1));/*call the helper function*/
 }
+
+/**
+ * is_palindrome_helper - function that checks if a string is a palindrome
+ *
+ * @s: pointer to string that we gonna check
+ * @beginning: start of string
+ * @end: end of string
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
 int is_palindrome_helper(char *s, int beginning, int end)
 {
-
-        if (beginning >= end) /*all characters compared*/
-                return (1);
-        else if (s[beginning] != s[end])
-                return (0);
+	if (beginning >= end) /*all characters compared*/
+		return (1);
+	else if (s[beginning] != s[end])
+		return (0);
 	else
 		return (is_palindrome_helper(s, beginning + 1, end - 1));
 }
@@ -41,9 +50,8 @@ int is_palindrome_helper(char *s, int beginning, int end)
 
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-
-        return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 
 }
