8a6f1f0de7c98af16432c45e781299eceff0c3e1
SOUAADJIA <souadouledjia@gmail.com>
2023-04-09 11:11:56 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: count the number of arguments passed to the program
+ * @argv: one-dimensional array of strings. Each string is one of
+ * the arguments that was passed to the program.
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("This program was called with %s\n", argv[0]);
+	return (0);
+}
+
