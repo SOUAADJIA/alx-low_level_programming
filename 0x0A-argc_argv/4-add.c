6af42a19efd12222de7fc7e83115951d143cb4b5
SOUAADJIA <souadouledjia@gmail.com>
2023-04-10 08:14:35 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - program that multiplies two numbers.
+ *
+ * @argc: count the number of arguments passed to the program
+ * @argv: one-dimensional array of strings. Each string is one of
+ * the arguments that was passed to the program.
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += _atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+/**
+ * is_positive_number - checks if a string is a positive number
+ *
+ * @str: string to check
+ *
+ * Return: 1 if str is a positive number, 0 otherwise
+ */
+
+int is_positive_number(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	while (*str != '\0')
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * _atoi - Convert a string to an integer.
+ *
+ * @s: The pointer to convert
+ *
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	int c = 0;
+	unsigned int i = 0;
+	int min = 1;
+	int j = 0;
+
+	while (s[c])
+	{
+		if (s[c] == '-')
+		{
+			min *= -1;
+		}
+
+		while (s[c] >= '0' && s[c] <= '9')
+		{
+			j = 1;
+			i = (i * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (j == 1)
+		{
+			break;
+		}
+		c++;
+	}
+
+	i *= min;
+	return (i);
+}
