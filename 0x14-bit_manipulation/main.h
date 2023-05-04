9ab84d1d46e4cd09e55048fae1fd974944528f60
Souaad OULED-JIA <souadouledjia@gmail.com>
2023-05-04 10:16:07 -0700

    0.0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+int _putchar(char c);
+int _strlen(const char *s);
+int _pow_recursion(int x, int y);
+
+
+
+#endif
