62150cb0b70880fe49cf13c12d8bf4bad4e01d48
Souaad OULED-JIA <souadouledjia@gmail.com>
2023-05-08 13:30:09 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: pointer to the name of the file
@@ -14,38 +16,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int FD;
 	char *buff;
 	size_t bytes_to_read;
 	size_t bytes_to_write;
-	FILE *fp;
 
 
-	fp = fopen(filename, O_RDONLY);
 	if (filename == NULL)
 		return (0);
-	if (fp == NULL)
+	
+	FD = open(filename, O_RDONLY);
+	
+	if (FD == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 	{
-		fclose(fp);
+		close(FD);
 		return (0);
 	}
-	bytes_to_read = fread(buff, 1, letters, fp);
+	bytes_to_read = read(FD, buff, letters);
+	close(FD);
 	if (bytes_to_read == 0)
 	{
-		fclose(fp);
 		free(buff);
 		return (0);
 	}
 
-	bytes_to_write = fwrite(buff, 1, bytes_to_read, stdout);
-	fclose(fp);
+	bytes_to_write = write(STDOUT_FILENO, buff, bytes_to_read);
 	free(buff);
 
 	if (bytes_to_write != bytes_to_read)
 		return (0);
-	return (bytes_to_read);
+	return (bytes_to_write);
 }
 
