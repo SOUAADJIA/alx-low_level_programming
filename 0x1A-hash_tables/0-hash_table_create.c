54e05e6ecff0d29b1051169450d3db737eb68e20
Souaad OULEDJIA <souadouledjia@gmail.com>
2023-10-06 09:25:16 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include "hash_tables.h"
 
 /**
@@ -9,20 +11,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t **new_array;
+	unsigned long int i;
 
-	new_table = calloc(1, sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
-	new_array = calloc(size, sizeof(hash_node_t *));
-	if (new_array == NULL)
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
 
-	new_table->array = new_array;
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
+
 	return (new_table);
 }
